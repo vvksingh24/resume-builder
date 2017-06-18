ee0d0114a423e427dcc698e598adfe6e6ab97b1f
vvksingh24 <vvksingh24@gmail.com>
2017-06-18 13:03:17 +0530

    changed url

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^bio/',include('cv.urls'))
+    url(r'^',include('cv.urls'))
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
